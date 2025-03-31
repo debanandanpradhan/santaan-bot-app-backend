9df545989d22d8e09e6aed47b9168a99f2b7dd1e
Debanandan Pradhan <122469371+debanandanpradhan@users.noreply.github.com>
2025-03-31 16:37:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 // Enable CORS
-app.use(cors({ 
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true 
-}));
+app.use(cors({ origin: "*" }));
 
 // JSON Middleware
 app.use(express.json());
